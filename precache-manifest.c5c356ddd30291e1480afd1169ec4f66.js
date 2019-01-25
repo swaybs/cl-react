4ea70d65eeb8d170d9d5cf05905ab8a04101bd43
Andrew Rothbauer <andrew.rothbauer@gmail.com>
2019-01-25 14:55:41 -0500

    Updates

---
+++ precache-manifest.c5c356ddd30291e1480afd1169ec4f66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "443173f59b106befe47b",
+    "url": "/cl-react/static/css/main.5d9d8b54.chunk.css"
+  },
+  {
+    "revision": "443173f59b106befe47b",
+    "url": "/cl-react/static/js/main.443173f5.chunk.js"
+  },
+  {
+    "revision": "02c362169c29ed4171a5",
+    "url": "/cl-react/static/js/1.02c36216.chunk.js"
+  },
+  {
+    "revision": "de556825d1daba023129",
+    "url": "/cl-react/static/js/runtime~main.de556825.js"
+  },
+  {
+    "revision": "57500b52a83516c2ac8c537e57291a45",
+    "url": "/cl-react/index.html"
+  }
+];
